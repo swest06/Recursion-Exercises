6ec739b33be35d76cff273154cd7961503ca6c55
Mr Sean West <swest06@dcs.bbk.ac.uk>
2017-11-23 21:54:59 +0000

    First attempt at ex02

--- ex02/power.py
+++ ex02/power.py
@@ -1,2 +1,11 @@
-def power(a,n):
-	return True
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def power(a,n):
+    x = n - 1
+    result = a
+    if n == 0:
+        result = 1
+    while x > 0: 
+        result = result*a
+        x = x - 1
+    return result
