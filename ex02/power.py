acb75a18d7d8eef57de195fab6415ead3b3de9a3
Sean West <swest06@dcs.bbk.ac.uk>
2017-11-24 12:01:13 +0000

    remove licence info etc from ex02

--- ex02/power.py
+++ ex02/power.py
@@ -1,5 +1,4 @@
-Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
+
 >>> def power(a,n):
     x = n - 1
     result = a
